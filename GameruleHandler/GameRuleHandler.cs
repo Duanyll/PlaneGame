d988735956f6540c4cbc3f700170ad1e8417ab24
duanyll <xiaoguo2004@sina.cn>
2018-08-01 17:04:20 +0800

    为GameBoard编写了构造函数

--- GameruleHandler/GameRuleHandler.cs
+++ GameruleHandler/GameRuleHandler.cs
@@ -6,6 +6,81 @@ using System.Threading.Tasks;
 
 namespace GameruleHandler
 {
+    /// <summary>
+    /// 表示一个格子的内容。受自定义规则影响，可能会有枚举中没有的取值
+    /// </summary>
+    public enum GameBoardBlock
+    {
+        Null,
+        Nothing,
+        Unknown
+    }
+    /// <summary>
+    /// 表示一个玩家的游戏板
+    /// </summary>
+    public class GameBoard
+    {
+        private List<List<GameBoardBlock>> Blocks;
+        public readonly int Width;
+        public readonly int Height;
+        /// <summary>
+        /// 获取某个特定格子的状态
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public GameBoardBlock this[int x,int y]
+        {
+            get
+            {
+                return Blocks[x][y];
+            }
+        }
+        /// <summary>
+        /// 构造一个具有指定大小的GameBoard
+        /// </summary>
+        /// <param name="w"></param>
+        /// <param name="h"></param>
+        public GameBoard(int w,int h)
+        {
+            Width = w;
+            Height = h;
+            Blocks = new List<List<GameBoardBlock>>();
+        }
+        /// <summary>
+        /// 从现有字符串数组加载GameBoard
+        /// </summary>
+        /// <param name="vs"></param>
+        public GameBoard(string[] vs)
+        {
+            Height = vs.Length;
+            for(int i = 0; i < Height; i++)
+            {
+                Width = Math.Min(Width, vs[i].Length);
+            }
+            for(int i = 0; i < Height; i++)
+            {
+                for(int j = 0; j < Width; j++)
+                {
+                    switch (vs[i][j])
+                    {
+                        case ' ':
+                            Blocks[i][j] = GameBoardBlock.Nothing;
+                            break;
+                        case '?':
+                            Blocks[i][j] = GameBoardBlock.Unknown;
+                            break;
+                        case '!':
+                            Blocks[i][j] = GameBoardBlock.Null;
+                            break;
+                        default:
+                            Blocks[i][j] = (GameBoardBlock)vs[i][j];
+                            break;
+                    }
+                }
+            }
+        }
+    }
     public class GameRuleHandler
     {
 
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # PlaneGame
+
 飞机游戏，或者是你们说的海战
+
+## 规划
+
+这次计划客户端与服务端二合一
+
+### PlaneGame.csproj
+
+主界面选单（水）
+
+### NetServer & NetClient
+
+负责网络通信，和DDZGame那个一毛一样
+
+### GameruleHandler
+
+用于处理游戏逻辑，集中式服务器
+
+### ServerGUI
+
+创建新游戏界面
+
+### ClientGUI
+
+游戏主界面
