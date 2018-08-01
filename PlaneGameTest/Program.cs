c4a5f997be2082c541f01038f6548486207a51dc
duanyll <xiaoguo2004@sina.cn>
2018-08-01 12:53:13 +0800

    添加了测试项目

---
+++ PlaneGameTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaneGameTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if !DEBUG
+            PlaneGame.App.Main();
+#else
+
+#endif
+        }
+    }
+}
