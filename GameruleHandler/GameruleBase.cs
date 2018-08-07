5a3cac63f6cf70b274054f1f08cd4c7773fa17b2
duanyll <xiaoguo2004@sina.cn>
2018-08-07 22:17:08 +0800

    把IGamerule接口改成类了

--- GameruleHandler/GameruleBase.cs
+++ GameruleHandler/GameruleBase.cs
@@ -10,18 +10,28 @@ namespace GameruleHandler
     /// <summary>
     /// 开局后（游戏选项设置完毕）游戏规则处理（包括等待玩家加入，玩家放置单位，正式开局等动作）的接口
     /// </summary>
-    public interface IGamerule
+    public abstract class GameruleBase
     {
-        NetworkServer Server { set; }
-        GameInfo Info { set; }
+        NetworkServer Server = new NetworkServer();
+        GameInfo Info { get; set; }
         /// <summary>
         /// 检测某个GameInfo是否符合当前模式要求
         /// </summary>
         /// <param name="info"></param>
         /// <returns></returns>
-        bool CheckGameInfo(GameInfo info);
-        void Work();
-        void StartGame();
-        void AbortGame();
+        public virtual bool CheckGameInfo(GameInfo info)
+        {
+            //TODO:完善Check
+            throw new NotImplementedException();
+        }
+        public abstract void Work();
+        public virtual void StartGame()
+        {
+
+        }
+        public virtual void AbortGame()
+        {
+
+        }
     }
 }
