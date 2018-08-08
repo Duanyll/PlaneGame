47cfa669d0e85f59f2936e1526bf55b54c255bb8
duanyll <xiaoguo2004@sina.cn>
2018-08-08 13:26:39 +0800

    添加了ClassicGamerule类

---
+++ GameruleHandler/ClassicGamerule.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameruleHandler
+{
+    class ClassicGamerule:GameruleBase
+    {
+        public override void StartGame()
+        {
+            if (Info == null)
+            {
+                throw new InvalidOperationException();
+            }
+            Server.MessageRecieved += Server_MessageRecieved;
+            Server.UserLoggedIn += Server_UserLoggedIn;
+            Server.UserLoggedOut += Server_UserLoggedOut;
+            Server.StartService();
+        }
+
+        private void Server_UserLoggedOut(string UserName)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Server_UserLoggedIn(string UserName)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Server_MessageRecieved(string UserName, string msg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void AbortGame()
+        {
+            if (MainThread != null)
+            {
+                if (MainThread.IsAlive == true)
+                {
+                    MainThread.Abort();
+                }
+            }
+            Server.StopService();
+        }
+
+        public override void Work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- GameruleHandler/GameruleBase.cs
+++ GameruleHandler/GameruleBase.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using NetServer;
 
 namespace GameruleHandler
@@ -12,8 +13,9 @@ namespace GameruleHandler
     /// </summary>
     public abstract class GameruleBase
     {
-        NetworkServer Server = new NetworkServer();
-        GameInfo Info { get; set; }
+        protected NetworkServer Server = new NetworkServer();
+        public GameInfo Info { get; set; }
+        protected Thread MainThread;
         /// <summary>
         /// 检测某个GameInfo是否符合当前模式要求
         /// </summary>
@@ -22,16 +24,21 @@ namespace GameruleHandler
         public virtual bool CheckGameInfo(GameInfo info)
         {
             //TODO:完善Check
-            throw new NotImplementedException();
+            if(info.Mask == null)
+            {
+                return false;
+            }
+            return true;
+            //throw new NotImplementedException();
         }
         public abstract void Work();
-        public virtual void StartGame()
-        {
-
-        }
-        public virtual void AbortGame()
-        {
-
-        }
+        /// <summary>
+        /// 启动网络服务，并在人数足够后开局
+        /// </summary>
+        public abstract void StartGame();
+        /// <summary>
+        /// 中止网络服务与游戏进程
+        /// </summary>
+        public abstract void AbortGame();
     }
 }
