751f154c888316b798c0aa552efc6405200aa31b
duanyll <xiaoguo2004@sina.cn>
2018-09-23 18:28:33 +0800

    重新设计了部分页面

--- PlaneGame/Properties/Resources.Designer.cs
+++ PlaneGame/Properties/Resources.Designer.cs
@@ -22,7 +22,7 @@ namespace PlaneGame.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources {
+    public class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace PlaneGame.Properties {
         ///   返回此类使用的缓存的 ResourceManager 实例。
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PlaneGame.Properties.Resources", typeof(Resources).Assembly);
@@ -51,7 +51,7 @@ namespace PlaneGame.Properties {
         ///   重写当前线程的 CurrentUICulture 属性。
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -61,9 +61,19 @@ namespace PlaneGame.Properties {
         }
         
         /// <summary>
+        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
+        /// </summary>
+        public static System.Drawing.Bitmap HomePageBG {
+            get {
+                object obj = ResourceManager.GetObject("HomePageBG", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   查找类似 https://github.com/Duanyll/PlaneGame/wiki 的本地化字符串。
         /// </summary>
-        internal static string OnineHelpURL {
+        public static string OnineHelpURL {
             get {
                 return ResourceManager.GetString("OnineHelpURL", resourceCulture);
             }
